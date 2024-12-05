2727f468ae7364a33a22e36c0de363415b0105b9
Ahmed Zedan <ahmedhamdizedan@gmail.com>
2024-12-05 06:03:56 +0200

    Update hero.tsx

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,7 +1,7 @@
 
 const HomeHero = () =>{
     return (
-        <section className="header h-screen w-full overflow-hidden bg-home-banner bg-no-repeat bg-contain bg-center mt-20  " id="header">  </section>
+        <section className="header h-screen w-full overflow-hidden bg-home-banner bg-top bg-no-repeat bg-contain bg-center mt-20" id="header">  </section>
     )
 }
 
