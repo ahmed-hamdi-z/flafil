fa1f93ee3ffea0f0d467dea7c0a75e6f3532379f
Click-Agency <clickck24@gmail.com>
2024-12-06 21:20:44 +0200

    add changes successfully

--- src/components/home/offers.tsx
+++ src/components/home/offers.tsx
@@ -28,7 +28,7 @@ const Offers: React.FC = () => {
   return (
     <div className="bg-[#1C4233] min-h-screen p-14 flex flex-col items-center">
       <h1 className="text-center text-3xl font-bold mb-16 mt-5 text-[#C8C6AF]">العروض</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:w-auto w-96 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:w-auto w-[350px] ">
         {offers.map((offer) => (
           <div
             key={offer.id}
@@ -38,7 +38,7 @@ const Offers: React.FC = () => {
             <img
               src={offer.image}
               alt={offer.title}
-              className="w-full  h-[70vh] object-cover transition-transform duration-300 group-hover:scale-105"
+              className="w-full h-[70vh] object-cover transition-transform duration-300 group-hover:scale-105"
             />
 
             {/* Hover Content */}
