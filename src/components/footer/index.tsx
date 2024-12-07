f8de8a5270705b6a6a30c62ba786fb05770930d8
Click-Agency <clickck24@gmail.com>
2024-12-07 19:51:39 +0200

    change footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -9,9 +9,9 @@ const Footer: React.FC = () => {
     return (
         <footer className="w-full h-[30vh] bg-gradient-to-br from-[#1C4233] to-[#486859] overflow-hidden ">
             <div className="md:flex items-center justify-center h-full gap-5 md:gap-20 text-lg space-y-4 pt-5">
-                <Link to="" className="flex items-center text-[#C8C6AF]"> <span className="mr-3"> <FaPhone className="w-10 h-6"/>  </span> +966530885799 </Link>
-                <Link to="" className="flex items-center text-[#C8C6AF]" > <span className="mr-3"> <MdAlternateEmail className="w-10 h-6"/>  </span> info@flafil.sa </Link>
-                <Link to="" className="flex items-center text-[#C8C6AF]" > <span className="mr-3"> <MdMyLocation className="w-10 h-6"/> </span> القصيم - عنيزه - حي المنار طريق الملك سعود </Link>
+                <Link type="tel:+966530885799" to="/" className="flex items-center text-[#C8C6AF] hover:text-[#8d8d8b]"> <span className="mr-3"> <FaPhone className="w-10 h-6"/>  </span> +966530885799 </Link>
+                <Link to="" className="flex items-center text-[#C8C6AF] hover:text-[#8d8d8b]" > <span className="mr-3"> <MdAlternateEmail className="w-10 h-6"/>  </span> info@flafil.sa </Link>
+                <Link to="https://maps.app.goo.gl/t7AThjeuwwVZXbWm9?g_st=com.google.maps.preview.copy" className="flex items-center text-[#C8C6AF] hover:text-[#8d8d8b]" > <span className="mr-3"> <MdMyLocation className="w-10 h-6"/> </span> القصيم - عنيزه - حي المنار طريق الملك سعود </Link>
             </div>
         </footer>
     );
--- src/components/home/meal-section.tsx
+++ src/components/home/meal-section.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const MenuSection: React.FC = () => {
   return (
-    <div className="relative h-screen  ">
+    <div className="relative h-screen md:mb-20">
 
       <div className="absolute inset-0 bg-[#1C4233] "></div>
 
@@ -24,7 +24,7 @@ const MenuSection: React.FC = () => {
           <h1 className="text-3xl md:text-3xl lg:text-6xl font-bold text-[#37594A]">
             أطباق شهية
           </h1>
-          <p className="text-2xl md:text-4xl text-[#37594A] font-semibold">
+          <p className="text-2xl md:text-4xl text-[#37594A] font-semibold pb-8">
             بأسعار مناسبة
           </p>
           <Link to='/menu' className="bg-gradient-to-t from-[#6b9985] to-[#C8C6AF] text-[#37594A] px-12 py-2 rounded-full text-2xl font-semibold border-b-4 border-[#37594A] ">
