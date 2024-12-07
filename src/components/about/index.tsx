73f9dc84a2e11de543876894b5d859df3e0f253c
Click-Agency <clickck24@gmail.com>
2024-12-07 19:21:18 +0200

    change contact

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,17 +1,17 @@
 const AboutComponent = () => {
     return (
-        <section className=" flex flex-col items-center justify-center mt-32">
+        <section className=" flex flex-col items-center justify-center mt-32 ">
             <h1 className="text-[#C8C6AF] text-6xl font-bold">من نحن </h1>
             <div className="md:w-[80%] lg:w-[90%] w-[90%] h-[100%] bg-[#C8C6AF] my-10 md:p-20 lg:p-20 p-3 text-center text-[#1C4233]" >
                 <h3 className="md:text-4xl lg:text-4xl text-2xl font-bold">فلافل وزنة الوجهة المثالية لعشاق الفلافل</h3>
-                <p className="md:text-3xl lg:text-3xl text-xl md:px-32 lg:px-32 px-3 font-semibold leading-loose pt-5 pb-32">حيث يمكنك الاستمتاع بتجربة نكهات بلا مثيل يقوم طهاتنا
+                <p className="md:text-3xl lg:text-3xl text-xl md:px-32 lg:px-32 px-3 font-semibold pt-5 pb-32 md:leading-loose lg:leading-loose ">حيث يمكنك الاستمتاع بتجربة نكهات بلا مثيل يقوم طهاتنا
                     الخبراء بتحضير كل طبق بدقة وشغف ليصنعوا تناغم
                     النكهات المثالي لنأخذك في عالم من الطعم الشهي والأجواء
                     الفاخرة، إن تحضيرنا المتفرد للفلافل وخياراتنا الواسعة
                     في هذا المجال تمنحكم تجربة تذوق لا تنسى بخدمة استثنائية</p>
             </div>
-            <img src="/images/about-mid.png" className="hidden md:block w-52 absolute left-10 top-64 " alt="" />
-            <img src="/images/about-mid.png" className="hidden md:block w-52 absolute right-10 top-96" alt="" />
+            <img src="/images/about-mid.png" className="hidden md:block w-52 absolute left-2 top-64" alt="" />
+            <img src="/images/about-mid.png" className="hidden md:block w-52 absolute right-2 top-96" alt="" />
             <img src="/images/about-bottom.png" className="md:w-[500px] lg:w-[500px] w-[350px] md:-mt-64 lg:-mt-64 -mt-44" alt="" />
 
         </section>
--- src/components/contact/form.tsx
+++ src/components/contact/form.tsx
@@ -1,11 +1,10 @@
 // Dependencies
 import { useRef, useState } from 'react';
-import { useTranslation } from "react-i18next";
 import emailjs from "@emailjs/browser";
 import { IoCloseSharp } from "react-icons/io5";
 
 const Form: React.FC = () => {
-    const { t } = useTranslation();
+
 
     const [message, setMessage] = useState<string | null>(null);
     const form = useRef<HTMLFormElement>(null);
@@ -32,77 +31,62 @@ const Form: React.FC = () => {
         }
     };
     return (
-        <div className=" w-full h-full flex flex-col-reverse items-center bg-[#1C4233]">
+        <div className=" w-full h-full flex justify-center items-center bg-[#1C4233] text-right">
             <form className=" md:w-[60%] w-[90%] my-24" ref={form} onSubmit={sendEmail}>
-                <h2 className="text-4xl md:text-7xl font-bold text-[#C8C6AF] text-center py-16"> {t("تواصل معنا")}</h2>
-    
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
-                    {/* First Name */}
-                    <div className="flex flex-col">
-                        <label htmlFor="firstName" className="text-[#C8C6AF] ">
-                            {t("First name")}
-                        </label>
-                        <input
-                            type="text"
-                            id="firstName"
-                            name="first_name"
-                            placeholder={t("First name")}
-                            className="mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
+                <h2 className="text-4xl md:text-7xl font-bold text-[#C8C6AF] text-center py-16">تواصل معنا</h2>
 
-                    {/* Last Name */}
-                    <div className="flex flex-col">
-                        <label htmlFor="lastName" className="text-[#C8C6AF]">
-                            {t("Last name")}
-                        </label>
-                        <input
-                            type="text"
-                            id="lastName"
-                            name="last_name"
-                            placeholder={t("Last name")}
-                            className="mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
+
+
+                <div className="mt-4">
+                    <label htmlFor="name" className="text-[#C8C6AF]">
+                        الاسـم
+                    </label>
+                    <input
+                        name="user_name"
+                        type="name"
+                        id="name"
+                        placeholder="الاسم"
+                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-right"
+                    />
                 </div>
 
                 {/* Email Address */}
                 <div className="mt-4">
                     <label htmlFor="email" className="text-[#C8C6AF]">
-                        {t("Email")}
+                        البريد الإلكتروني
                     </label>
                     <input
                         name="user_email"
                         type="email"
                         id="email"
-                        placeholder={t("Email")}
-                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="البريد الإلكتروني"
+                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-right"
                     />
                 </div>
                 {/* Phone */}
                 <div className="mt-4">
                     <label htmlFor="phone" className="text-[#C8C6AF]">
-                        {t("Phone")}
+                        رقم الجوال
                     </label>
                     <input
                         type="tel"
                         id="country"
                         name="phone"
-                        placeholder={t("Phone")}
-                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="رقم الجوال"
+                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-right"
                     />
                 </div>
 
                 {/* Notes */}
                 <div className="mt-4">
                     <label htmlFor="notes" className="text-[#C8C6AF]">
-                        {t("Notes")}
+                        رسالتك
                     </label>
                     <textarea
                         id="notes"
                         name="message"
-                        placeholder={t("Notes")}
-                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="رسالتك"
+                        className="w-full mt-2 p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-right"
                     />
                 </div>
 
@@ -110,7 +94,7 @@ const Form: React.FC = () => {
                 <div className="mt-6 flex justify-center">
                     <button type="submit"
                         className="bg-[#C8C6AF] text-[#1C4233] text-2xl px-12 py-2.5 rounded-full hover:bg-teal-800 transition duration-300">
-                        {t("Submit")}
+                        إرسال
                     </button>
                 </div>
             </form>
@@ -135,13 +119,13 @@ const Form: React.FC = () => {
                             onClick={() => setMessage(null)}
                             className="bg-white text-[#764095] px-6 py-2 rounded-full font-semibold hover:bg-[#764095] hover:text-white transition-colors duration-200 ease-in-out shadow-md"
                         >
-                            {t("Close")}
+                            Close
                         </button>
                     </div>
                 </div>
 
             )}
-        
+
         </div>
     )
 };
